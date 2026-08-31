ad7c1abc14018244e71a9b2bdd825f04793c4eb6
ADNAN MUNIR <engradnanmunir338@gmail.com>
2026-08-31 15:06:40 +0200

    Update inference_Instruction_finetuning.py

--- inference_Instruction_finetuning.py
+++ inference_Instruction_finetuning.py
@@ -9,8 +9,6 @@ import os
 import argparse
 import torch
 
-os.environ["HF_HOME"]            = "/mimer/NOBACKUP/groups/naiss2024-22-1298/Adnan/hf_cache"
-os.environ["TRANSFORMERS_CACHE"] = "/mimer/NOBACKUP/groups/naiss2024-22-1298/Adnan/hf_cache"
 
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 
